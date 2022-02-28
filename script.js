8513b3c14b5f23a80e8e10a4dce5b758f3e14f87
buhariaminat <buhariaminat@yahoo.com>
2022-02-28 22:17:07 +0100

    Update style.css

---
+++ script.js
@@ -0,0 +1,11 @@
+const display = document.querySelector(".display-menu");
+const menu = document.querySelector(".menu");
+const hamburger = document.querySelector(".hamburger-menu");
+const close = document.querySelector(".close");
+const nav = document.querySelector(".nav-bar-list");
+const list = document.querySelectorAll("nav-list");
+
+function onClick () {
+    if 
+
+}
