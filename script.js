5a0dbac9195bacd8f3dee9aafedd44794aa7b90a
buhariaminat <buhariaminat@yahoo.com>
2022-03-01 14:12:08 +0100

    Update index.html

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-const display = document.querySelector(".display-menu");
-const menu = document.querySelector(".menu");
-const hamburger = document.querySelector(".hamburger-menu");
-const close = document.querySelector(".close");
-const nav = document.querySelector(".nav-bar-list");
-const list = document.querySelectorAll("nav-list");
 
-function onClick () {
-    if 
 
-}
