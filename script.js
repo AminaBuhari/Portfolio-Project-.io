14e759b37050f0e06cdd8fe9082e48a63f052a09
buhariaminat <buhariaminat@yahoo.com>
2022-03-02 18:38:53 +0100

    Update index.html

--- script.js
+++ script.js
@@ -17,42 +17,77 @@ navMobileList.forEach((elem)=>{
     elem.addEventListener('click', toggler);
 })
 
-const popup= [
-    {
-    header: 'Multi-Post Stories Gain+Glory',
-    languages: ["Ruby on rails", "css", "javascript","Html"],
-    live: ["https://aminabuhari.github.io/Portfolio-Project-.io/"],
-    source: ["https://github.com/AminaBuhari/Portfolio-Project-.io"],
-}
+const projects = [
+    {   id: 1, 
+        name: 'Multi-Post Stories Gain+Glory',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+        featured_image: "images/Snapshoot Portflio.png",
+        technologies: ["Ruby on rails", "css", "javascript", "html"],
+        live: "https://aminabuhari.github.io/Portfolio-Project-.io/",
+        source: "https://github.com/AminaBuhari/Portfolio-Project-.io",
+    },
+    {   id: 2, 
+        name: 'Multi-Post Stories Gain+Glory',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+        featured_image: "images/Snapshoot Portflio.png",
+        technologies: ["Ruby on rails", "css", "javascript", "html"],
+        live: "https://aminabuhari.github.io/Portfolio-Project-.io/",
+        source: "https://github.com/AminaBuhari/Portfolio-Project-.io",
+    },
+    {   id: 3, 
+        name: 'Multi-Post Stories Gain+Glory',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+        featured_image: "images/Snapshoot Portflio.png",
+        technologies: ["Ruby on rails", "css", "javascript", "html"],
+        live: "https://aminabuhari.github.io/Portfolio-Project-.io/",
+        source: "https://github.com/AminaBuhari/Portfolio-Project-.io",
+    },
+    {   id: 4, 
+        name: 'Multi-Post Stories Gain+Glory',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+        featured_image: "images/Snapshoot Portflio.png",
+        technologies: ["Ruby on rails", "css", "javascript", "html"],
+        live: "https://aminabuhari.github.io/Portfolio-Project-.io/",
+        source: "https://github.com/AminaBuhari/Portfolio-Project-.io",
+    },
+    {   id: 5, 
+        name: 'Multi-Post Stories Gain+Glory',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+        featured_image: "images/Snapshoot Portflio.png",
+        technologies: ["Ruby on rails", "css", "javascript", "html"],
+        live: "https://aminabuhari.github.io/Portfolio-Project-.io/",
+        source: "https://github.com/AminaBuhari/Portfolio-Project-.io",
+    },
+    {   id: 6, 
+        name: 'Multi-Post Stories Gain+Glory',
+        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea',
+        featured_image: "images/Snapshoot Portflio.png",
+        technologies: ["Ruby on rails", "css", "javascript", "html"],
+        live: "https://aminabuhari.github.io/Portfolio-Project-.io/",
+        source: "https://github.com/AminaBuhari/Portfolio-Project-.io",
+    },
+]
+   
 
-];
 
-const newContainer = document.getElementsByClassName("first-class");
-newContainer.innerHTML = `
-<div class="container-3">
-<div class="container-4">
-  <div class="container-5">
-    <h4 class="header-3">Multi-Post Stories Gain+Glory</h4>
-  </div>
-  <div>
-    <ul class="button-1">
-      <li class="list">
-        <button class="button-1h">Ruby on rails</button>
-      </li>
-      <li class="list">
-        <button class="button-1h">css</button>
-      </li>
-      <li class="list">
-        <button class="button-1h">Javascript</button>
-      </li>
-      <li class="list">
-        <button class="button-1h">html</button>
-      </li>
-    </ul>
-  </div>
-  <div class="buttons-2">
-    <button class="button-2h">See Project</button>
-  </div>
-</div>
-
-`;
+window.onload = () => {
+    const newContainer = document.getElementById("class");
+    projects.forEach((project) => {
+      const cardsHTML= `<div class="container-3">
+      <div class="container-4" id="${project.id}">
+        <div class="container-5">
+          <h4 class="header-3">${project.name}</h4>
+        </div>
+        <div>
+          <ul class="button-1">
+          ${project.technologies.map((technology) => `<li class="list"><button class="button-1h">${technology}</button></li>`).join('')}
+          </ul>
+        </div>
+        <div class="buttons-2">
+          <button class="button-2h" type="button" onclick="">See Project</button>
+        </div>
+      </div>
+      `; 
+      newContainer.insertAdjacentHTML('beforeend', cardsHTML)
+    })
+};
